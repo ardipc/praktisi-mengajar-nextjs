f8fa8d19dd8ffc67be308751d05ec4c7fb8d706f
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2023-05-16 15:03:49 +0700

    update

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,42 @@
-import Head from "next/head";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-
-export default function Header(props) {
-    const { title, keywords, description } = props;
+const Header = () => {
     return (
-        <Head>
-            <title>{title}</title>
-            <meta name="keywords" content={keywords} />
-            <meta name="description" content={description} />
-        </Head>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#">Ardiansyah</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="#action1">Beranda</Nav.Link>
+                    <Nav.Link href="#action2">Kategori</Nav.Link>
+                    <Nav.Link href="#">
+                        Produk Baru
+                    </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                    <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                    <Button variant="outline-primary mx-2">Masuk</Button>
+                </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
+
+export default Header;
--- public/js/main.js
+++
@@ -1,1 +0,0 @@
-console.log('Run file main.js')
